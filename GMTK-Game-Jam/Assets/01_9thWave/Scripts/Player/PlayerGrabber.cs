c15808d5d35fa06d6fdd473c1ac8bbd5ba141dee
Juliusz Trochimczuk <88376167+juliuszTrochimczuk@users.noreply.github.com>
2025-08-01 19:26:37 +0200

    Feature/better movement (#6)
    * Better player movement with camera controller
    
    * Making camera work better

---
+++ GMTK-Game-Jam/Assets/01_9thWave/Scripts/Player/PlayerGrabber.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using static UnityEngine.InputSystem.InputAction;
+
+namespace _01_9thWave.Scripts.Player
+{
+    public class PlayerGrabber : MonoBehaviour
+    {
+        [SerializeField] private Transform _holdPoint;
+        [SerializeField] private Transform _grabCheck;
+        [SerializeField] private float _gradRadius;
+        [SerializeField] private LayerMask _whatIsGrabbable;
+        [SerializeField] private float _grabDistance = 3f;
+        [SerializeField] private float _grabMagnitude = 2f;
+        [SerializeField] private float _normalGravityScale = 5f;
+
+        private Rigidbody2D _heldObject;
+        private MousePoint _mousePoint;
+
+        private void Awake()
+        {
+            _mousePoint = GetComponentInChildren<MousePoint>();
+        }
+
+
+        private void FixedUpdate()
+        {
+            if (_heldObject == null)
+                return;
+
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, _mousePoint.transform.position, _grabDistance, 8);
+            if (hit.collider != null)
+                _holdPoint.position = hit.point;
+            else
+                _holdPoint.transform.position = (_mousePoint.transform.position - transform.position).normalized * _grabDistance + transform.position;
+
+            _heldObject.gravityScale = 0f;
+            _heldObject.velocity = (_holdPoint.position - _heldObject.transform.position) * _grabMagnitude;
+        }
+
+        public void Grab(CallbackContext ctx)
+        {
+            if (ctx.performed)
+            {
+                if (_heldObject == null)
+                {
+                    Collider2D[] hitColliders = Physics2D.OverlapCircleAll(transform.position, _gradRadius, _whatIsGrabbable);
+
+                    if (hitColliders.Length <= 0)
+                        return;
+
+                    _heldObject = hitColliders[0].gameObject.GetComponent<Rigidbody2D>();
+                    _heldObject.gameObject.layer = LayerMask.NameToLayer("GrabbedObject");
+                }
+                else
+                {
+                    _heldObject.gameObject.layer = LayerMask.NameToLayer("MovableObject");
+                    _heldObject.gravityScale = _normalGravityScale;
+                    _heldObject = null;
+                }
+            }
+        }
+    }
+}
---
+++ GMTK-Game-Jam/Assets/01_9thWave/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+using static UnityEngine.InputSystem.InputAction;
+
+namespace _01_9thWave.Scripts.Player
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        [SerializeField] private UnityEvent onJump;
+        [SerializeField] private UnityEvent onLanding;
+        [SerializeField] private UnityEvent<float> onChangingDirection;
+
+        [Header("Walking")]
+        [SerializeField] private float _groundMaxSpeed;
+        [SerializeField] private float _groundMoveSmoother;
+
+        [Header("Jumping")]
+        [SerializeField] private float _inAirMaxSpeed;
+        [SerializeField] private float _inAirMoveSmoother;
+        [SerializeField] private float maxTimeInAir;
+        [SerializeField] private float _jumpForce;
+        [SerializeField] private AnimationCurve _jumpCurve;
+
+        private CircleCollider2D _collider;
+        private Rigidbody2D _rb;
+
+        private float _verticalVelocity;
+        private float _horizontalVelocity;
+        private float _currentHorizontalVelocity;
+
+        public float InputDirection { get; private set; }
+        private float _MaxSpeed
+        {
+            get
+            {
+                if (_onGround)
+                    return _groundMaxSpeed;
+                else
+                    return _inAirMaxSpeed;
+            }
+        }
+        private float _MoveSmoother
+        {
+            get
+            {
+                if (_onGround)
+                    return _groundMoveSmoother;
+                else
+                    return _inAirMoveSmoother;
+            }
+        }
+        private bool _onGround;
+
+        private void Awake()
+        {
+            _rb = GetComponent<Rigidbody2D>();
+            _collider = GetComponent<CircleCollider2D>();
+        }
+
+        private void FixedUpdate()
+        {
+            IsPlayerOnGround();
+
+            _horizontalVelocity = Mathf.SmoothDamp(_horizontalVelocity, InputDirection, ref _currentHorizontalVelocity, _MoveSmoother);
+            _rb.velocity = new Vector2(_horizontalVelocity * _MaxSpeed, _verticalVelocity);
+        }
+
+        public void ReadMoveInputVector(CallbackContext ctx)
+        {
+            InputDirection = ctx.ReadValue<float>();
+            onChangingDirection.Invoke(InputDirection);
+        }
+
+        public void Jump(CallbackContext ctx)
+        {
+            if (_onGround && ctx.performed)
+                StartCoroutine(Jumping());
+        }
+
+        private IEnumerator Jumping()
+        {
+            float time = 0.0f;
+            onJump.Invoke();
+            do
+            {
+                _verticalVelocity = _jumpForce * _jumpCurve.Evaluate(time / maxTimeInAir);
+
+                time += Time.fixedDeltaTime;
+                yield return new WaitForFixedUpdate();
+            }
+            while (time <= maxTimeInAir || !_onGround);
+            onLanding.Invoke();
+            _verticalVelocity = 0;
+        }
+
+        private void IsPlayerOnGround() =>
+            _onGround = Physics2D.Raycast(transform.position, Vector2.down, _collider.radius + 0.05f, LayerMask.GetMask("Default", "Ground"));
+    }
+}
