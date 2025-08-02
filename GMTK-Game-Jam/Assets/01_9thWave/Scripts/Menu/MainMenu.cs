22537a5ed466e7931c2f95cfaadb1377bd6fc449
Red <mark.matveyenka@proton.me>
2025-08-02 18:16:32 +0200

    Added main menu sound effects

--- GMTK-Game-Jam/Assets/01_9thWave/Scripts/Menu/MainMenu.cs
+++ GMTK-Game-Jam/Assets/01_9thWave/Scripts/Menu/MainMenu.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using _01_9thWave.Scripts.Audio;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,8 +8,16 @@ namespace Menu
 {
     public class MainMenu : MonoBehaviour
     {
-        public void LoadScene(string sceneName) => SceneManager.LoadScene(sceneName);
+        public void LoadScene(string sceneName)
+        {
+            AudioManager.Instance.PlayClickSound();
+            SceneManager.LoadScene(sceneName);
+        }
 
-        public void ExitApp() => Application.Quit();
+        public void ExitApp()
+        {
+            AudioManager.Instance.PlayClickSound();
+            Application.Quit();
+        }
     }
 }
