5895233e37e47fb9ec76201c6849f47364614e40
Red <mark.matveyenka@proton.me>
2025-08-02 15:30:01 +0200

    Added audio manager

---
+++ GMTK-Game-Jam/Assets/01_9thWave/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+namespace _01_9thWave.Scripts.Audio
+{
+    public class AudioManager : MonoBehaviour
+    {
+        [SerializeField] private AudioMixer _audioMixer;
+        
+        [Header("Background Music")]
+        [SerializeField] private AudioSource _backgroundMusicSource;
+        
+        public void SetMasterVolume(float volume) { _audioMixer.SetFloat("masterVolume", volume); }
+    
+        public void SetSoundEffectsVolume(float volume) { _audioMixer.SetFloat("soundEffectsVolume", volume); }
+    
+        public void SetMusicVolume(float volume) { _audioMixer.SetFloat("musicVolume", volume); }
+
+        public void PlayBackgroundMusic() { _backgroundMusicSource.Play(); }
+
+        public void StopBackgroundMusic() { _backgroundMusicSource.Stop(); }
+        
+        public void SetBackgroundMusicSource(AudioSource musicSource) { _backgroundMusicSource = musicSource; }
+    }
+}
