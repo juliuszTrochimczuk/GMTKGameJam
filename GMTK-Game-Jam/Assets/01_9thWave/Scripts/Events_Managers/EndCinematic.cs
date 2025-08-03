10220d28198bd79503d1fd72d317787d35d79a7f
gonerik <s28728@pjwstk.edu.pl>
2025-08-03 18:27:53 +0200

    Cinematic and the credits

---
+++ GMTK-Game-Jam/Assets/01_9thWave/Scripts/Events_Managers/EndCinematic.cs
@@ -0,0 +1,54 @@
+﻿namespace _01_9thWave.Scripts.Events_Managers
+{
+    using UnityEngine;
+    using UnityEngine.SceneManagement;
+    using UnityEngine.Video;
+
+    [RequireComponent(typeof(VideoPlayer))]
+    public class VideoEndOrSkip : MonoBehaviour
+    {
+        private VideoPlayer videoPlayer;
+
+        void Start()
+        {
+            videoPlayer = GetComponent<VideoPlayer>();
+
+            if (videoPlayer != null)
+            {
+                videoPlayer.loopPointReached += OnVideoFinished;
+            }
+            else
+            {
+                Debug.LogError("VideoPlayer component not found on the object.");
+            }
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                LoadNextScene();
+            }
+        }
+
+        void OnVideoFinished(VideoPlayer vp)
+        {
+            LoadNextScene();
+        }
+
+        void LoadNextScene()
+        {
+            int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+            int totalScenes = SceneManager.sceneCountInBuildSettings;
+
+            if (currentSceneIndex + 1 < totalScenes)
+            {
+                SceneManager.LoadScene(currentSceneIndex + 1);
+            }
+            else
+            {
+                SceneManager.LoadScene(0); // Loop back to the first scene
+            }
+        }
+    }
+}
